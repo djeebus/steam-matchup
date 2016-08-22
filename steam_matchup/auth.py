88b96913889cd6c02180621132019afbaab112e8
Joe Lombrozo <joe@djeebus.net>
2016-08-21 22:54:32 -0700

    Auth changes

--- steam_matchup/auth.py
+++ steam_matchup/auth.py
@@ -1,6 +1,7 @@
 from urllib import parse
 
 from pyramid.httpexceptions import HTTPTemporaryRedirect
+from pyramid_openid import verify_openid
 
 
 def includeme(config):
@@ -9,17 +10,29 @@ def includeme(config):
     """
 
     config.add_route('auth: openid', '/')
-    from pyramid_openid import verify_openid
-    config.add_view(route_name='auth: openid', view=verify_openid)
+    config.add_view(route_name='auth: openid', view=on_begin_auth)
+
+
+def on_begin_auth(context, request):
+    redirect_url = request.GET.get('redirect_url')
+    if redirect_url:
+        request.session['redirect_url'] = redirect_url
+
+    response = verify_openid(context, request)
+    return response
 
 
 def on_success(context, request, openid_data):
     identity_url = openid_data['identity_url']
     url_parts = parse.urlparse(identity_url)
+    profile_id = url_parts.path.split('/')[-1]
 
-    response = HTTPTemporaryRedirect('/')
+    dashboard_url = request.session.get('redirect_url', '/')
+    dashboard_url += '?' + profile_id
+
+    response = HTTPTemporaryRedirect(dashboard_url)
     response.set_cookie(
-        'profile_id', url_parts.path.split('/')[-1],
+        'profile_id', profile_id,
     )
 
     return response
